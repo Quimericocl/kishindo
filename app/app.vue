695d3444c88593802eb054c0e013012221e1f8ce
Wato77 <gonzalo_edc@hotmail.com>
2025-01-17 18:35:04 -0300

    feat: Site in construction

--- app/app.vue
+++ app/app.vue
@@ -5,9 +5,15 @@ useSeoMeta({
 </script>
 
 <template>
-	<div class="grid min-h-screen place-items-center supports-[(min-height:100svh)]:min-h-100svh">
-		<h1 class="text-center text-8 font-bold">
-			Construyendo Kishindo
-		</h1>
+	<div class="grid min-h-screen place-items-center bg-dark supports-[(min-height:100svh)]:min-h-100svh">
+		<div>
+			<img src="assets/logos/Kishindo.png" alt="Kishindo logo" class="h-350px">
+			<h1 class="text-center text-8 text-white font-bold">
+				Kishindo
+			</h1>
+			<h2 class="text-center text-5 text-white/50">
+				Sitio en construcción
+			</h2>
+		</div>
 	</div>
 </template>
