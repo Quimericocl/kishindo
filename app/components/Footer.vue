8fac6415594647bb15f426d769c8be4c94e7827b
Wato77 <gonzalo_edc@hotmail.com>
2025-01-17 01:04:10 -0300

    fix: components in the correct place now

---
+++ app/components/Footer.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+	<div class="h-200px w-full flex items-center self-end justify-center bg-dark px-5 py-2">
+		<!-- Kishindo Logos -->
+		<div class="flex flex-col place-items-center">
+			<img src="~/assets/logos/Kishindo.png" alt="Kishindo Logo" width="100" class="mb-2">
+			<div class="flex justify-center gap-x-2">
+				<img src="~/assets/logos/Karate.png" alt="Kishindo - Karate Logo" width="30">
+				<img src="~/assets/logos/Kendo.png" alt="Kishindo - Kendo Logo" width="30">
+				<img src="~/assets/logos/Reiki.png" alt="Kishindo - Reiki Logo" width="30">
+			</div>
+		</div>
+
+		<!-- Federation Logos -->
+		<div class="flex justify-center gap-x-2">
+			<img src="~/assets/logos/ShodokanChile.png" alt="Kishindo - Karate Logo" width="30">
+			<img src="~/assets/logos/ShodokanOkinawa.png" alt="Kishindo - Kendo Logo" width="30">
+		</div>
+	</div>
+</template>
